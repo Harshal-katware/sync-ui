2d9c5cdd9828a23b24ee13e6a8a62e5467070890
Harshal-katware <harshal_hk.starsoftech@outlook.com>
2026-04-11 17:02:39 +0530

    routing changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
-import React from 'react'
-import InventoryManagement from './Inventory/Inventorymanagement '
 
-function App() {
-  return (
-    <div>
-       <InventoryManagement/>
-    </div>
-  )
-}
 
-export default App
-// App.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
+import RestaurantPOS from './BILLING/Restaurantpos';
+import InventoryManagement from './Inventory/Inventorymanagement ';
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        
+      
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/inventory" element={<InventoryManagement />} />
+        <Route path="/billing" element={<RestaurantPOS />} />
       </Routes>
     </BrowserRouter>
   );
